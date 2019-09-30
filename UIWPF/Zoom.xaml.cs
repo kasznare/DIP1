9ad1e3634ad94076e989407ddc71b55e0f828de3
András Kasznár <andras.kasznar@in-ex.hu>
2019-09-30 21:38:15 +0200

    added the test Zoom project, too

---
+++ UIWPF/Zoom.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UIWPF {
+    /// <summary>
+    /// Interaction logic for Zoom.xaml
+    /// </summary>
+    public partial class Zoom : Window {
+        public Zoom() {
+            InitializeComponent();
+
+            ZoomViewbox.Width = 100;
+            ZoomViewbox.Height = 100;
+        }
+
+        private void MainWindow_OnMouseWheel(object sender, MouseWheelEventArgs e) {
+            UpdateViewBox((e.Delta > 0) ? 5 : -5);
+        }
+
+        private void UpdateViewBox(int newValue) {
+            if ((ZoomViewbox.Width >= 0) && ZoomViewbox.Height >= 0) {
+                ZoomViewbox.Width += newValue;
+                ZoomViewbox.Height += newValue;
+            }
+        }
+    }
+}
+
