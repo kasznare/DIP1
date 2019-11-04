cdafa5d01469db8334ece4b85a9f56e2646c93f2
András Kasznár <andras.kasznar@in-ex.hu>
2019-11-04 20:05:22 +0100

    new filed

---
+++ Generate/Simulation/Simulation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Simulation {
+    /// <summary>
+    /// this class is resposible for managing the simulation steps
+    /// </summary>
+    public class Simulation {
+        public void DetermineSimulationStep() {
+
+        }
+
+        public void ExecuteOperation(IOperation op, OperationParameters parameters) {
+
+        }
+    }
+
+    public class SwitchOperation : IOperation
+    {
+        public void SimulationStep()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class MoveOperation : IOperation {
+        public void SimulationStep()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public interface IOperation
+    {
+        void SimulationStep();
+    }
+
+    public class OperationParameters {
+    }
+}
---
+++ UIWPF/ViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp1;
+
+namespace UIWPF {
+    public class ViewModel
+    {
+        public Model m;
+
+        public ViewModel()
+        {
+            
+        }
+    }
+}
