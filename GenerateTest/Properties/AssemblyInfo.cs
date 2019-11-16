74c0701ad5b88df93cd3544719163059a11281a7
András Kasznár <andras.kasznar@in-ex.hu>
2019-11-16 18:14:44 +0100

    last real step

---
+++ GenerateTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GenerateTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GenerateTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4c14c388-80f1-4dfe-8927-d73835cd9843")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ GenerateTest/SimpleModelTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using WindowsFormsApp1;
+using WindowsFormsApp1.Simulation;
+using NUnit;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace GenerateTest {
+    public class SimpleModelTest {
+
+        Simulate s = new Simulate();
+        [SetUp]
+        public void Setup() {
+            s.model = new Model();
+            s.model.InitSimpleModel();
+
+        }
+        //the goal of the tests are to avoid exceptions, if there is an exception, it fails.
+        [Test]
+        public void FullSimulation() {
+            s.run();
+        }
+
+        [Test]
+        public void SwitchTest() {
+            Assert.AreEqual(10, 10);
+        }
+
+        [TearDown]
+        public void Teardown() {
+            s = null;
+        }
+    }
+}
